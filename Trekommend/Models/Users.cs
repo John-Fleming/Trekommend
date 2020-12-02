fc29bf672d224495a473334ef12c8b2dbfe2be14
John Fleming <johndeclanfleming@gmail.com>
2020-12-01 21:23:05 -0600

    Add project files.

---
+++ Trekommend/Models/Users.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trekommend.Models
+{
+    public class Users
+    {
+        public int UserId { get; set; }
+        public int Uuid { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+        public DateTime DateJoined { get; set; }
+        public string UserPhoto { get; set; }
+    }
+}
