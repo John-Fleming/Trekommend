186ac08a0ac71006479cc914728ad5d45a9f5361
John Fleming <johndeclanfleming@gmail.com>
2020-12-02 21:15:30 -0600

    added navbar component and page components routes to app.js

--- trekommend.ui/src/App/App.js
+++ trekommend.ui/src/App/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import './App.scss';
 
+import {
+  BrowserRouter,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+
 import UserProfile from '../components/pages/UserProfile/UserProfile';
+import MyNavbar from '../components/shared/MyNavbar/MyNavbar';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <UserProfile></UserProfile>
+        <BrowserRouter>
+          <React.Fragment>
+            <MyNavbar></MyNavbar>
+              <Switch>
+                <Route path='/profile' component={UserProfile}/>
+                {/* <Route path='/trips/:tripId' component={SingleTrip}/>
+                <Route path='/recommendation/:recommendationId' component={SingleRecommendation}/>
+                <Route path='/trips' component={Trips}/> */}
+                {/* <Route path='/discover' component={SearchResults}/> */}
+                {/* <Route path='/login' component={Login}/> */}
+                {/* <Route path='/' component={Home} /> */}
+                <Redirect from="*" to="/" />
+              </Switch>
+          </React.Fragment>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ trekommend.ui/src/components/shared/MyNavbar/MyNavbar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { NavLink as RRNavLink } from 'react-router-dom';
+
+import {
+  Navbar,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+
+import './MyNavbar.scss';
+
+class MyNavbar extends React.Component {
+  render() {
+    return (
+      <div className="MyNavbar">
+          <Navbar color="light" light>
+            <NavLink tag={RRNavLink} to="/home" className="brand-header navbar-brand">Trekommend</NavLink>
+              <Nav className="ml-auto flex-row" navbar>
+                <NavItem className="mr-2">
+                  <NavLink tag={RRNavLink} to="/profile"><i className="fas fa-search text-dark"></i></NavLink>
+                </NavItem>
+                <NavItem className="mx-2">
+                  <NavLink tag={RRNavLink} to="/profile"><i className="fas fa-plus text-dark"></i></NavLink>
+                </NavItem>
+                <NavItem className="ml-2">
+                  <NavLink tag={RRNavLink} to="/profile"><i className="far fa-user text-dark"></i></NavLink>
+                </NavItem>
+              </Nav>
+          </Navbar>
+        </div>
+    );
+  }
+}
+
+export default MyNavbar;
