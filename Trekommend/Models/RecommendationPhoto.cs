4e31e659f18eb517457af1feaa05d0fd6174ea96
John Fleming <johndeclanfleming@gmail.com>
2020-12-05 13:19:55 -0600

    rec photos api set up

---
+++ Trekommend/Models/RecommendationPhoto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trekommend.Models
+{
+    public class RecommendationPhoto
+    {
+        public int PhotoId { get; set; }
+        public int RecId { get; set; }
+        public string PhotoUrl { get; set; }
+    }
+}
