da565e044bbae8b5b6d1208bcbdd4ddc9e558fb7
John Fleming <johndeclanfleming@gmail.com>
2020-12-05 12:35:42 -0600

    rec categories api set up

---
+++ Trekommend/Models/Recommendation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trekommend.Models
+{
+    public class Recommendation
+    {
+        public int RecId { get; set; }
+        public int UserId { get; set; }
+        public int TripId { get; set; }
+        public int RecCategoryId { get; set; }
+        public string Title { get; set; }
+        public int? Rating { get; set; }
+        public string Review { get; set; }
+        public string Description { get; set; }
+        public int TimesSaved{ get; set; }
+    }
+}
---
+++ Trekommend/Models/RecommendationCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trekommend.Models
+{
+    public class RecommendationCategory
+    {
+        public int RecCategoryId { get; set; }
+        public string Type { get; set; }
+    }
+}
