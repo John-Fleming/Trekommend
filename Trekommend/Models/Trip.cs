26e5405954fd1dc2e7d4dba16806afd769c83399
John Fleming <johndeclanfleming@gmail.com>
2020-12-05 12:15:21 -0600

    trips api set up

---
+++ Trekommend/Models/Trip.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trekommend.Models
+{
+    public class Trip
+    {
+        public int TripId { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string CoverPhoto { get; set; }
+        public bool IsPlanned { get; set; }
+    }
+}
