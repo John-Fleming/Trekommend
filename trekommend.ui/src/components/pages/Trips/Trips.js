0a6204b9362173b27d72c0a9405ca74a4a946fb6
John Fleming <johndeclanfleming@gmail.com>
2021-01-04 19:23:48 -0600

    conditionally render add trip button

--- trekommend.ui/src/components/pages/Trips/Trips.js
+++ trekommend.ui/src/components/pages/Trips/Trips.js
@@ -6,6 +6,7 @@ import AddOrEditTripForm from '../../shared/AddOrEditTripForm/AddOrEditTripForm'
 
 import UserData from '../../../helpers/data/UserData';
 import TripData from '../../../helpers/data/TripData';
+import AuthData from '../../../helpers/data/AuthData';
 
 class Trips extends React.Component {
   state = {
@@ -13,11 +14,17 @@ class Trips extends React.Component {
     trips: [],
     tripFormModal: false,
     editingTrip: false,
+    isAuthedUser: false,
   }
 
   getUser = (userId) => {
     UserData.getUserByUserId(userId)
-      .then((resp) => this.setState({ user: resp }))
+      .then((resp) => {
+        this.setState({ user: resp });
+        const authedUserUid = AuthData.getUid();
+
+        if (resp.uuid === authedUserUid) this.setState({ isAuthedUser: true });
+      })
       .catch((err) => console.error('could not get user object', err));
   }
 
@@ -47,6 +54,7 @@ class Trips extends React.Component {
       trips,
       tripFormModal,
       editingTrip,
+      isAuthedUser,
     } = this.state;
 
     const buildTripCards = trips.map((trip, index) => <TripCard key={index} trip={trip} user={user}/>);
@@ -56,8 +64,12 @@ class Trips extends React.Component {
         <span className="Trips-header">
           {user.firstName} {user.lastName}'s Trips [{trips.length}]
         </span>
-        {/* to do: only render the below button if the user in state equals the authed user */}
-        <button className="btn" onClick={this.toggleTripFormModal}><i className="fas fa-plus"></i></button>
+
+        { isAuthedUser
+          ? <button className="btn" onClick={this.toggleTripFormModal}><i className="fas fa-plus"></i></button>
+          : ''
+        }
+
         <div className="Trips-container">
           {buildTripCards}
         </div>
